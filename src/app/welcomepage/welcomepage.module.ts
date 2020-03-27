0570734a456666c9d71a39de2b3097863b201fba
hamdirhibi <geekrhibi@gmail.com>
2020-03-27 02:39:16 +0100

    spinner page

--- src/app/welcomepage/welcomepage.module.ts
+++ src/app/welcomepage/welcomepage.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule ,CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
 import { IonicModule } from '@ionic/angular';
 
 import { WelcomepagePage } from './welcomepage.page';
-
+import 'gl-ionic-background-video';
+import { TranslateModule } from '@ngx-translate/core';
 const routes: Routes = [
   {
     path: '',
@@ -19,8 +19,11 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    TranslateModule.forChild(), 
+
     RouterModule.forChild(routes)
   ],
-  declarations: [WelcomepagePage]
+  declarations: [WelcomepagePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] // here is the schema declaration to add
 })
 export class WelcomepagePageModule {}
--- src/app/welcomepage/welcomepage.page.ts
+++ src/app/welcomepage/welcomepage.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import 'gl-ionic-background-video';
 
 @Component({
   selector: 'app-welcomepage',
@@ -10,12 +11,15 @@ export class WelcomepagePage implements OnInit {
 
   constructor(private router : Router) { }
 
-  async ngOnInit() {
-    await this.sleep(3000);
-          this.router.navigate(['/tabs/tabs/tab1']); 
+  ngOnInit() {
+   // await this.sleep(60000);
+      //  this.router.navigate(['/tabs/tabs/tab1']); 
 
     }
     sleep(ms) {
       return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    gohome(){
+      this.router.navigate(['/tabs/tabs/tab1']); 
     }
 }
